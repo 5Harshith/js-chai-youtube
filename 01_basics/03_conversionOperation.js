07ef3280ca43474c71b20e336052442b4936707f
Harshith k <86103962+5Harshith@users.noreply.github.com>
2024-07-07 15:51:49 +0000

    conversion of data

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+let score =232;
+
+console.log(typeof(score));
+console.log (typeof score);
+
+let valueInNumber =String(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=> 33
+//"33adc"=> NaN - not a number
+//let valueInNumber =boolen(score);
+//1=>true ; 0=>fasle
+//""=>false
+//"hai"=>true
