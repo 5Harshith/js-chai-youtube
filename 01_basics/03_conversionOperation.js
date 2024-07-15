74270701757b8a4f30cfe470025d819606e18dfb
Harshith k <86103962+5Harshith@users.noreply.github.com>
2024-07-15 09:32:50 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score =232;
 
-console.log(typeof(score));
-console.log (typeof score);
+// console.log(typeof(score));
+// console.log (typeof score);
 
 let valueInNumber =String(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"33"=> 33
 //"33adc"=> NaN - not a number
@@ -13,3 +13,8 @@ console.log(valueInNumber);
 //1=>true ; 0=>fasle
 //""=>false
 //"hai"=>true
+
+// ******* operations **************//
+let age=18
+let negValu =-age
+console.log(negValu);
