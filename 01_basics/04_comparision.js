a6698a5ce628978b246046e72bd966aad94cd1c4
Harshith k <86103962+5Harshith@users.noreply.github.com>
2024-07-15 09:49:19 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+// console.log(2>1)
+// console.log("2">10)
+// console.log("2"===2)// it will not convert the data type
+// also it will check for the data type
+console.log(null>0);
+console.log(null <0)
+console.log(null>=0) 
+console.log("undefined")
+console.log(undefined>0);
+console.log(undefined <0)
+console.log(undefined >=0) 
